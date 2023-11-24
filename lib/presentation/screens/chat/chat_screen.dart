2a72a7ccac8fef4cd0efe2a4de186f42d412387d
Alex S <alexs1010@gmail.com>
2023-11-24 15:24:11 -0600

    Create flutter new project, add screens of main page and set with dummy data, added widgets to simulate chat screen

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0KXeOUG0MPRAIhQLZkvSpMFf84vq2ZMzgLHJ_SCZ6JhXn_B2tHMkiQdZ6cnCSfB3H5tE&usqp=CAU'),
+          ),
+        ),
+        title: const Text("Hello love"),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {  
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return( index % 2 == 0) ? MyMessageBubble() : HerMessageBubble();
+              },)
+            ),
+            Text('Hola'),
+            Text('Mundo'),
+          ],      
+        ),
+      ),
+    );
+  }
+}
