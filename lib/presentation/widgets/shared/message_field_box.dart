693e6d944edc9934e4c5a0d01f20e20ebe8af0bf
Alex S <alexs1010@gmail.com>
2023-11-27 12:28:06 -0600

    Finish screens and interface with basic interaction, add text form closed #1 and finished details

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,29 +5,39 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
 
     final colors = Theme.of(context).colorScheme;
     final outlineInputBorder = UnderlineInputBorder(
-      borderSide: const BorderSide(color: Colors.transparent),
+      borderSide: BorderSide(color: colors.primary),
       borderRadius: BorderRadius.circular(40),
     );
     final inputDecoration = InputDecoration(
-        enabledBorder: outlineInputBorder,
-        focusedBorder: outlineInputBorder,
-        filled: true,
-        suffixIcon: IconButton(
-           icon: const Icon(Icons.send_outlined),
+      hintText: 'End your message with "?"',
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send_outlined),
           onPressed: () {
+            final textValue = textController.value.text;
+            print('boton $textValue');
+            textController.clear();
         }),
     );
 
-    return TextFormField(
+    return TextFormField(  
+      onTapOutside: (event){
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value){
         print('submit value $value');
-      },
-      onChanged: (value){
-        print('changed: $value');
+        textController.clear();
+        focusNode.requestFocus();
       },
     );
   }
